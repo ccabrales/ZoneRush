7e0c7981f3e6d059af475b28b9864e024479db6e
Xiaonan Tong <xiaonan@stanford.edu>
2015-11-08 12:42:44 -0800

    Bullets?

---
+++ src/BulletType.cpp
@@ -0,0 +1,9 @@
+//
+//  BulletType.cpp
+//  ZoneRushMaxima
+//
+//  Created by Xiaonan Tong on 11/8/15.
+//
+//
+
+#include "BulletType.hpp"
---
+++ src/BulletType.hpp
@@ -0,0 +1,26 @@
+//
+//  BulletType.hpp
+//  ZoneRushMaxima
+//
+//  Created by Xiaonan Tong on 11/8/15.
+//
+//
+
+#ifndef BulletType_hpp
+#define BulletType_hpp
+
+#include <stdio.h>
+#include "ofMain.h"
+
+struct BulletType {
+    ofVec2f aim;
+    float angle; //radians or degrees tbd.
+    ofImage img;
+};
+
+class BulletLibrary{
+public:
+    static BulletType
+};
+
+#endif /* BulletType_hpp */
