5464455ad29116fbc5361a86c59df4caed60e75b
Xiaonan Tong <xiaonan@stanford.edu>
2015-11-09 16:51:42 -0800

    hmm this looks like it works?

--- src/BulletType.h
+++ src/BulletType.h
@@ -9,8 +9,41 @@ struct BulletType {
     ofImage* img;
 };
 
-static struct BulletLibrary {
-    static BulletType StraightShot = (BulletType){ ofVec2f(-1.0, 0.0), 0, new ofImage() };
-//    static BulletType 
+
+class BulletLibrary
+{
+public:
+    static BulletLibrary& getInstance( ){
+        static BulletLibrary inst;
+        return inst;
+    };
     
+    BulletType StraightShot[1] = {BulletType{ ofVec2f(-1.0, 0.0), 0, new ofImage() }};
+    BulletType ClusterShot[3] = {
+        BulletType{ofVec2f(-0.4, 0.4), 45, new ofImage() },
+        BulletType{ofVec2f(-0.5656, 0), 0, new ofImage() },
+        BulletType{ofVec2f(-0.4, -0.4), -45, new ofImage() }
+    };
+    
+
+private:
+    BulletLibrary();
+    
+    static BulletLibrary* instance;
+    
+};
+
+
+class Logger{
+public:
+    static Logger* Instance();
+    bool openLogFile(std::string logFile);
+    void writeToLogFile();
+    bool closeLogFile();
+    
+private:
+    Logger(){};  // Private so that it can  not be called
+    Logger(Logger const&){};             // copy constructor is private
+    Logger& operator=(Logger const&){};  // assignment operator is private
+    static Logger* m_pInstance;
 };
